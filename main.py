9d88aa48a98ea4d6fdb17b4324fa748e965a1477
DennisMalm <dennis.malm@gmail.com>
2021-06-28 18:31:41 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,40 +1,76 @@
+import threading
+import wave
+
+import pyaudio
+from inputimeout import inputimeout, TimeoutOccurred
+from timeit import default_timer
 from playsound import playsound
-from threading import Thread
-import time
-
-looping = False
-last_sound_played = None
-
-def play(sound):
-    playsound(f'{sound}.wav', block=False)
+from sounds import sounds
 
 
-def loop_sound(sound):
-    global looping
-    for i in range(4):
-        playsound(f'{sound}.wav', block=False)
-        time.sleep(4)
-    looping = False
-
-
-def search(sound, sound_grid):
-    for row in sound_grid:
+def valid_sound(sound):
+    for row in sounds:
         for ele in row:
             if ele == sound:
                 return True
     return False
 
 
+def loop_child(sound, start, length, loops):
+    wave_file = wave.open(f'{sound}.wav', 'rb')
+
+    py_audio = pyaudio.PyAudio()
+    stream = py_audio.open(format=py_audio.get_format_from_width(wave_file.getsampwidth()),
+                           channels=wave_file.getnchannels(),
+                           rate=wave_file.getframerate(),
+                           output=True)
+    # skip unwanted frames
+    n_frames = int(start * wave_file.getframerate())
+    wave_file.setpos(n_frames)
+
+    # write desired frames to audio buffer
+    n_frames = int(length * wave_file.getframerate())
+    frames = wave_file.readframes(n_frames)
+    stream.write(frames)
+
+    for _ in range(loops):
+        stream.write(frames)
+
+    # close and terminate everything properly
+    stream.close()
+    py_audio.terminate()
+    wave_file.close()
+
+
+def loop_one(q, start, length, loops):
+    print(f'Looping sound: {q}')
+    threading.Thread(target=loop_child, args=(q, start, length, loops)).start()
+
+
 if __name__ == '__main__':
     while True:
-        sound_to_play = input("Scan a barcode right now:")
-        print(search(sound_to_play, sounds))
-
-        if search(sound_to_play, sounds):
-            # loop_sound(f'{sound_to_play}')
-            if sound_to_play == last_sound_played:
-                loop_sound(sound_to_play)
-            playsound(f'{sound_to_play}.wav', block=False)
-            last_sound_played = sound_to_play
-        else:
-            print('No such sound.')
+        print(f'Threads: {threading.activeCount()}')
+        try:
+            sound_one = inputimeout(prompt='First: ', timeout=1)
+            if valid_sound(sound_one):
+                try:
+                    sound_two = inputimeout(prompt='Second: ', timeout=0.5)
+                    try:
+                        sound_three = inputimeout(
+                            prompt='Third: ', timeout=0.5)
+                        try:
+                            sound_four = inputimeout(
+                                prompt='Fourth: ', timeout=0.5)
+                            if sound_four:
+                                print('>>>>>>>>> Third loop')
+                                loop_one(sound_four, 6, 0.125, 24)
+                        except TimeoutOccurred:
+                            print('>>>>>> Second loop')
+                            loop_one(sound_three, 4, 0.5, 12)
+                    except TimeoutOccurred:
+                        print('>>> First loopen')
+                        loop_one(sound_two, 2, 1.5, 4)
+                except TimeoutOccurred:
+                    playsound(f'{sound_one}.wav', block=False)
+        except TimeoutOccurred:
+            continue
